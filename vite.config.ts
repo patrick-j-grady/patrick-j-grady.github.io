61371dc08def8fb792eb794ec6e3566f5e3ae2d8
Patrick Grady <70718881+patrick-j-grady@users.noreply.github.com>
2024-01-22 11:08:43 -0800

    Removed base for production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  base: '/patrick-j-grady.github.io/',
+  //base: '/patrick-j-grady.github.io/',
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
